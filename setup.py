8dc856f8e770b12d86af4cd74eed5ae40bd3987c
sirhashir <umairhashir89@gmail.com>
2023-04-03 15:25:57 +0530

    Structure for project

--- setup.py
+++ setup.py
@@ -1,5 +1,23 @@
 #responsible to create the machine learning application as a package
 from setuptools import find_packages,setup
+from typing import List
+
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    """
+    this function will return list of requirements
+    
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
 
 setup(
 name="ml_project",
@@ -7,5 +25,5 @@ version="0.0.1",
 author="Hashir",
 author_email="umairhashir89@gmail.com",
 packages=find_packages(),
-install_requires = ['pandas', 'seaborn', 'numpy']
+install_requires = get_requirements('requirements.txt')
 )
