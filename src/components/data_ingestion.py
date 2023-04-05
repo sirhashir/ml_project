3aca9fdde0011ff209cea968d39f384e09e4dc1b
sirhashir <umairhashir89@gmail.com>
2023-04-05 14:51:22 +0530

    Data ingestion created

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -17,5 +17,40 @@ from dataclasses import dataclass
 
 @dataclass #we wil be able to directly define class variable
 class DataIngestionConfig: #any input we require will be given thorugh this class
-    pass
+    train_data_path: str=os.path.join('artifacts',"train.csv")# artifact is folder;  #data ingestion will save everything on this path
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"raw.csv")
 
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig() # as sson as we call this, the three path above will be saved in the ingestion_config class variable
+    
+    def initiate_data_ingestion(self): #use to write code to read from databases
+        logging.info("Entered the data ingestion method/component...")
+        try:
+            df = pd.read_csv('notebook\data\stud.csv')
+            logging.info("Read the dataset as dataframe")
+        
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True) #so that we can also save it using rawdatapath
+
+            logging.info("Train Test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=40)
+            
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info("Ingestion of data is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
+        
--- src/exception.py
+++ src/exception.py
@@ -22,10 +22,10 @@ class CustomException(Exception):
         return self.error_message
     
 
-if __name__=="__main__":
+# if __name__=="__main__":
 
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info("Divide by zero error")
-        raise CustomException(e, sys)
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         logging.info("Divide by zero error")
+#         raise CustomException(e, sys)
